c9a3e13b0399faee19ffc73ec3c11a4e87e3fd80
OlzhasOK <oljabay1@gmail.com>
2023-12-18 11:14:14 +0600

    небольшие доработки

--- lesson_3/task.py
+++ lesson_3/task.py
@@ -3,6 +3,7 @@
 Попробовать получить данные и сохранить в apple.json ТОЛЬКО те смартфоны у 
 которых память 256Гб ИЗ ПЕРВЫХ 20 смартфонов.
 """
+
 import json 
 import requests
 from bs4 import BeautifulSoup
@@ -13,12 +14,19 @@ url = 'https://www.technodom.kz/catalog/smartfony-i-gadzhety/smartfony-i-telefon
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-smartphones = soup.find_all('div', class_='ProductCardV_titleWrapper__a_HQC')
+smartphones = soup.find_all('li', class_='ProductList_item__9Jj7j')
 
 for smartphone in smartphones[:20]:
+    smartphone_url = smartphone.find('a', class_='ProductItem_itemLink__QPKXB')
     memory = smartphone.find('p', class_='Typography ProductCardV_title__rFAYr ProductCardV_loading__TkTOe Typography__M')
-    if memory and '256' in memory.text:
-        apple_256gb.append({'phone': memory.text})
-
+    
+    if smartphone_url and memory and '256' in memory.text:
+        phone_data = {
+            'phone': memory.text,
+            'link': f'https://www.technodom.kz{smartphone_url["href"]}'
+        }
+        apple_256gb.append(phone_data)
+        
 with open('./lesson_3/apple.json', 'w', encoding='utf-8') as file:
     json.dump(apple_256gb, file, ensure_ascii=False, indent=2)
+
